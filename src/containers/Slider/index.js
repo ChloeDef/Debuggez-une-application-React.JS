0b907df55cb280d0d79019945a3982bcc8a2236c
Chloe Defin <chloe.defin@gmail.com>
2024-10-08 19:28:55 +0200

    Slider fix v1

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,31 +1,35 @@
 import { useEffect, useState } from "react";
+import { fr } from "date-fns/locale"; // npm install date-fns
+import { format } from "date-fns"; // npm install date-fns
 import { useData } from "../../contexts/DataContext";
-import { getMonth } from "../../helpers/Date";
+// import { getMonth } from "../../helpers/Date";
 
 import "./style.scss";
 
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-  );
+  const byDateDesc = data?.focus
+  .sort((evtA, evtB) => (new Date(evtA.date) < new Date(evtB.date) ? -1 : 1))
+  .reverse();
+
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex(index < byDateDesc.length -1 ? index + 1 : 0),
       5000
     );
   };
+
   useEffect(() => {
     nextCard();
   });
+
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
+
+          <div key={event.title}>
+            <div className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
           >
@@ -34,7 +38,9 @@ const Slider = () => {
               <div className="SlideCard__description">
                 <h3>{event.title}</h3>
                 <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
+                                <div>
+                  {format(new Date(event.date), "MMMM", { locale: fr })}
+                </div>
               </div>
             </div>
           </div>
@@ -50,7 +56,7 @@ const Slider = () => {
               ))}
             </div>
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
